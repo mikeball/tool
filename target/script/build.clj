591d6cb558423f6859171d0eebbb70a2b2482124
Shaun Williams <shaun.e.williams@gmail.com>
2017-01-29 16:26:53 -0600

    add colors to figwheel errors/warnings

--- target/script/build.clj
+++ target/script/build.clj
@@ -1,7 +1,8 @@
 (require '[cljs.build.api :as b]
          '[clojure.edn :as edn]
          '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
-         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]])
+         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
+         '[strictly-specking-standalone.ansi-util :refer [with-color]])
 
 (def build (edn/read-string (first *command-line-args*)))
 
@@ -9,10 +10,11 @@
 
 (let [start (System/nanoTime)
       {:keys [src compiler]} build]
-  (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
-    (try
-      (b/build src
-        (assoc compiler :watch-error-fn #(print-exception %)))
-      (catch Throwable e
-        (print-exception e)))
+  (with-color
+    (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
+      (try
+        (b/build src
+          (assoc compiler :watch-error-fn #(print-exception %)))
+        (catch Throwable e
+          (print-exception e))))
     (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds")))
--- target/script/watch.clj
+++ target/script/watch.clj
@@ -1,11 +1,13 @@
 (require '[cljs.build.api :as b]
          '[clojure.edn :as edn]
          '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
-         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]])
+         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
+         '[strictly-specking-standalone.ansi-util :refer [with-color]])
 
 (def build (edn/read-string (first *command-line-args*)))
 
 (let [{:keys [src compiler]} build]
-  (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
-    (b/watch src
-      (assoc compiler :watch-error-fn #(print-exception %)))))
+  (with-color
+    (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
+      (b/watch src
+        (assoc compiler :watch-error-fn #(print-exception %))))))
