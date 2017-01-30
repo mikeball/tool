cb81da430c374b52ea28012c3260ba30e7dd9185
Shaun Williams <shaun.e.williams@gmail.com>
2017-01-30 01:12:59 -0600

    use base jars instead of base deps to save ~4s load time

--- src/tool/core.cljs
+++ src/tool/core.cljs
@@ -16,10 +16,6 @@
   [[:compiler :optimizations]
    [:compiler :target]])
 
-(defn base-deps []
-  {:dependencies [['org.clojure/clojurescript (:cljs-version config)]]
-   :dev-dependencies [['figwheel-sidecar "0.5.8"]]})
-
 ;; filenames
 (def file-config-edn "cljs.edn")
 (def file-deps-cache ".deps-cache.edn")
@@ -27,6 +23,12 @@
 (def file-build (str js/__dirname "/script/build.clj"))
 (def file-watch (str js/__dirname "/script/watch.clj"))
 (def file-repl (str js/__dirname "/script/repl.clj"))
+
+(defn file-cljs-jar [version] (str js/__dirname "/cljs-" version ".jar"))
+(defn url-cljs-jar [version] (str "https://github.com/clojure/clojurescript/releases/download/r" version "/cljs.jar"))
+
+(defn file-fig-jar [version] (str js/__dirname "/figwheel-sidecar-" version ".jar"))
+(def fig-version "0.5.8")
 
 ;;---------------------------------------------------------------------------
 ;; Lumo
@@ -61,6 +63,14 @@
   (or (java-installed?)
       (exit-error "Please install Java.")))
 
+(defn ensure-cljs-version! []
+  (let [version (:cljs-version config)
+        jar-path (file-cljs-jar version)
+        jar-url (url-cljs-jar version)]
+    (or (io/path-exists? jar-path)
+        (do (println "Downloading ClojureScript version" version)
+            (io/download jar-url jar-path)))))
+
 (defn ensure-config! []
   (or (io/slurp-edn file-config-edn)
       (exit-error "No config found. Please create one in" file-config-edn)))
@@ -71,14 +81,9 @@
 
 (declare task-install)
 
-(defn get-current-deps []
-  (let [user-deps (select-keys config dep-keys)
-        all-deps (merge-with concat (base-deps) user-deps)]
-    all-deps))
-
 (defn ensure-dependencies! []
   (let [cache (io/slurp-edn file-deps-cache)
-        stale? (not= (get-current-deps)
+        stale? (not= (select-keys config dep-keys)
                      (select-keys cache dep-keys))]
     (if stale?
       (task-install)
@@ -90,7 +95,7 @@
 
 (defn task-install []
   (ensure-java!)
-  (let [deps (apply concat (map (get-current-deps) dep-keys))
+  (let [deps (apply concat (map config dep-keys))
         result (spawn-sync "java"
                  #js["-jar" file-dep-retriever (pr-str deps)]
                  #js{:stdio #js["pipe" "pipe" 2]})
@@ -105,18 +110,22 @@
         (io/spit file-deps-cache (with-out-str (pprint cache)))
         cache))))
 
-(defn build-classpath [src]
+(defn build-classpath [src & {:keys [with-base-jars]}]
   (let [{:keys [jars]} (ensure-dependencies!)
         source-paths (when src (if (sequential? src) src [src]))
+        cljs-jar (file-cljs-jar (:cljs-version config))
+        fig-jar (file-fig-jar fig-version)
+        jars (cond->> jars with-base-jars (concat [cljs-jar fig-jar]))
         all (concat jars source-paths)
         sep (if windows? ";" ":")]
     (string/join sep all)))
 
 (defn task-script [id file-script]
   (ensure-java!)
-  (let [build (ensure-build! id)]
+  (let [build (ensure-build! id)
+        cp (build-classpath (:src build) :with-base-jars true)]
     (spawn-sync "java"
-      #js["-cp" (build-classpath (:src build)) "clojure.main" file-script (pr-str build)]
+      #js["-cp" cp "clojure.main" file-script (pr-str build)]
       #js{:stdio "inherit"})))
 
 (defn all-sources []
@@ -128,15 +137,16 @@
 
 (defn task-repl [id]
   (ensure-java!)
-  (spawn-sync "java"
-    #js["-cp" (build-classpath (all-sources)) "clojure.main" file-repl]
-    #js{:stdio "inherit"}))
+  (let [cp (build-classpath (all-sources) :with-base-jars true)]
+    (spawn-sync "java"
+      #js["-cp" cp "clojure.main" file-repl]
+      #js{:stdio "inherit"})))
 
 (defn task-custom-script [path user-args]
   (ensure-java!)
-  (let [classpath (build-classpath (all-sources))
+  (let [cp (build-classpath (all-sources) :with-base-jars true)
         onload (str "(do (def ^:dynamic *cljs-config* (quote " config ")) nil)")
-        args (concat ["-cp" classpath "clojure.main" "-e" onload path] user-args)]
+        args (concat ["-cp" cp "clojure.main" "-e" onload path] user-args)]
     (spawn-sync "java" (clj->js args) #js{:stdio "inherit"})))
 
 (defn print-welcome []
@@ -153,6 +163,7 @@
     (do
       (print-welcome)
       (set! config (ensure-config!))
+      (ensure-cljs-version!)
       (cond
         (= task "install") (task-install)
         (= task "build") (task-script (first args) file-build)
