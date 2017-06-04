319f4fa4a5956fd0d3b13ee9f8711abcf25eec81
Shaun Williams <shaun.e.williams@gmail.com>
2017-06-04 17:19:02 -0500

    simplify build and watch scripts

--- target/script/build.clj
+++ target/script/build.clj
@@ -1,18 +1,23 @@
-(require '[cljs.build.api :as b]
-         '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
-         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
-         '[strictly-specking-standalone.ansi-util :refer [with-color]])
+(require
+  ;; the ClojureScript compiler
+  '[cljs.build.api :as b]
 
+  ;; borrowing pretty error printing from Figwheel
+  '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
+  '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
+  '[strictly-specking-standalone.ansi-util :refer [with-color]])
+
+(def start (System/nanoTime))
 (println "Building" (:id *build-config*) "...")
 
-(let [start (System/nanoTime)
-      {:keys [src compiler]} *build-config*
+(let [{:keys [src compiler]} *build-config*
       source (if (sequential? src) (apply b/inputs src) src)
-      opts (assoc compiler :watch-error-fn #(print-exception %))]
+      opts (assoc compiler
+             :warning-handlers [(warning-message-handler identity)])]
   (with-color
-    (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
-      (try
-        (b/build source opts)
-        (catch Throwable e
-          (print-exception e))))
-    (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds")))
+    (try
+      (b/build source opts)
+      (catch Throwable e
+        (print-exception e)))))
+
+(println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds")
--- target/script/watch.clj
+++ target/script/watch.clj
@@ -1,11 +1,16 @@
-(require '[cljs.build.api :as b]
-         '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
-         '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
-         '[strictly-specking-standalone.ansi-util :refer [with-color]])
+(require
+  ;; the ClojureScript compiler
+  '[cljs.build.api :as b]
+
+  ;; borrowing pretty error printing from Figwheel
+  '[figwheel-sidecar.cljs-utils.exception-parsing :refer [print-exception]]
+  '[figwheel-sidecar.build-middleware.notifications :refer [warning-message-handler]]
+  '[strictly-specking-standalone.ansi-util :refer [with-color]])
 
 (let [{:keys [src compiler]} *build-config*
       source (if (sequential? src) (apply b/inputs src) src)
-      opts (assoc compiler :watch-error-fn #(print-exception %))]
+      opts (assoc compiler
+             :warning-handlers [(warning-message-handler identity)]
+             :watch-error-fn #(print-exception %))]
   (with-color
-    (binding [cljs.analyzer/*cljs-warning-handlers* [(warning-message-handler identity)]]
-      (b/watch source opts))))
+    (b/watch source opts)))
